07f087d171e0a6622664381514ca0c992c915259
ShadowHachiman <106827686+ShadowHachiman@users.noreply.github.com>
2025-10-07 16:13:24 -0300

    feat: Initial commit, base Angular 17 structure and feature folders

---
+++ Fornt-Final-V3/src/app/accounts/account-tree/account-tree.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-tree',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-tree.component.html',
+  styleUrl: './account-tree.component.css'
+})
+export class AccountTreeComponent {
+
+}
---
+++ Fornt-Final-V3/src/app/users/user-from/user-from.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-from',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-from.component.html',
+  styleUrl: './user-from.component.css'
+})
+export class UserFromComponent {
+
+}
